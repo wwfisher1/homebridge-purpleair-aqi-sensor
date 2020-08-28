3b40701de6b471540817bf9fd18499535a4c35bf
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-08-28 13:27:44 -0700

    Improve the measurement avg code

--- index.js
+++ index.js
@@ -40,20 +40,19 @@ PurpleAirAccessory.prototype = {
 	 * Get all Air data from PurpleAir
 	 */
 	getPurpleAirData: function () {
-		var self = this;
-		var url = 'https://www.purpleair.com/json?show=' + this.purpleID;
-
 		// Make request every updateFreq seconds (PurpleAir actual update frequency is around 40 seconds, but we really don't need that precision here}
 		// this.log("getPurpleAirData called... lastupdate: %s, now: %s, freq: %s", this.lastupdate.toString(), timenow.toString(), this.updateMsecs.toString());
+		var self = this;
 		request({
-			url: url,
+			url: 'https://www.purpleair.com/json?show=' + this.purpleID,
 			json: true,
 		}, function (err, response, data) {
 			// If no errors
-			if (!err && (response.statusCode === 200)) {
+			if (!err && response.statusCode === 200) {
 				self.updateData(data);
 				// If error
-			} else {
+			}
+			else {
 				purpleAirService.setCharacteristic(Characteristic.StatusFault, 1);
 				self.log.error("PurpleAir Network or Unknown Error.");
 			};
@@ -64,90 +63,46 @@ PurpleAirAccessory.prototype = {
 	 * Update data
 	 */
 	updateData: function (data) {
-		var self = this;
 		purpleAirService.setCharacteristic(Characteristic.StatusFault, 0);
+
 		// PurpleAir outdoor sensors send data from two internal sensors, but indoor sensors only have one
 		// We have to verify exterior/interior, and if exterior, whether both sensors are working or only 1
-		var statsA; //  = undefined;
-		var statsB;
+		var statsA;
+		var statsB = { lastModified: 0 };
 		var newest = 0;
-		var single = null;
-		if (data.results != undefined) {
-			if (data.results[0] != undefined) {
-				// stats[0] = undefined
-				if (data.results[0].Stats != undefined) statsA = JSON.parse(data.results[0].Stats);
-				if (data.results[0].DEVICE_LOCATIONTYPE != 'inside') {
-					// outside sensor, check for both sensors and find the one updated most recently
-					if (data.results[1] != undefined) {
-						//stats[1] = undefined
-						if (data.results[1].Stats != undefined) statsB = JSON.parse(data.results[1].Stats);
-						if (statsA.lastModified > statsB.lastModified) {		// lastModified is epoch time in milliseconds
-							newest = statsA.lastModified;
-						} else {
-							newest = statsB.lastModified;
-						}
-					}
-				} else {
-					// indoor sensor - make sure the data is valid
-					// stats[1] = undefined;
-					if ((data.results[0].A_H != true) && (data.results[0].PM2_5Value != undefined) && (data.results[0].PM2_5Value != null)) {
-						single = 0;
-						newest = statsA.lastModified;
-					} else {
-						single = -1;
-					}
-				}
-				if (newest == this.lastupdate) { // no change
-					// nothing changed
-					return;
-				}
-				// Now, figure out which PM2_5Value we are using
-				if (single == null) {
-					if ((data.results[0].A_H == true) || (data.results[0].PM2_5Value == undefined) || (data.results[0].PM2_5Value == null)) {
-						// A is bad
-						if ((data.results[1].A_H == true) || (data.results[1].PM2_5Value == undefined) || (data.results[1].PM2_5Value == null)) {
-							// A bad, B bad
-							single = -1;
-						} else {
-							// A bad, B good
-							single = 1;
-						}
-					} else {
-						// Channel A is good
-						if ((data.results[1].A_H == true) || (data.results[1].PM2_5Value == undefined) || (data.results[1].PM2_5Value == null)) {
-							// A good, B bad
-							single = 0;
-						} else {
-							// A good, B good
-							single = 2;
-						}
-					}
-				}
 
-				var pm
-				var aqi
-				if (single >= 0) {
-					if (single == 2) {
-						pm = Math.round(((statsA[self.statsKey] + statsB[self.statsKey]) / 2.0) * 100) / 100;
-					} else if (single == 0) {
-						pm = Math.round(statsA[self.statsKey] * 100) / 100;
-					} else {
-						pm = Math.round(statsB[self.statsKey] * 100) / 100;
-					}
-					pm = self.adjustPM(pm);
-					aqi = Math.round(self.calculateAQI(pm));
-				} else {
-					// No valid data
-					return
-				}
-				purpleAirService.setCharacteristic(Characteristic.PM2_5Density, pm.toString());
-				purpleAirService.setCharacteristic(Characteristic.AirQuality, self.transformAQI(aqi));
-
-				self.log.info("PurpleAir pm2_5 is %s, AQI is %s, Air Quality is %s.", pm.toString(), aqi.toString(), self.airQualityString(aqi));
-
-				self.lastupdate = newest;		// Use the newest sensors' time
-			}
+		// Basic sanity check
+		if (!data.results || !data.results[0] || !data.results[0].Stats) {
+			return;
 		}
+
+		statsA = JSON.parse(data.results[0].Stats);
+		if (data.results[1] && data.results[1].Stats && data.results[0].DEVICE_LOCATIONTYPE !== 'inside') {
+			statsB = JSON.parse(data.results[1].Stats);
+		}
+
+		newest = Math.max(statsA.lastModified, statsB.lastModified);
+		if (newest <= this.lastupdate) {
+			return;
+		}
+		this.lastupdate = newest;
+
+		var va = statsA[this.statsKey];
+		var vb = statsB[this.statsKey];
+		if (va && vb) {
+			va = (va + vb) / 2;
+		}
+		else if (!va) {
+			va = vb || 0;
+		}
+
+		var pm = this.adjustPM(va);
+		var aqi = this.calculateAQI(pm);
+
+		purpleAirService.setCharacteristic(Characteristic.PM2_5Density, pm.toFixed(2));
+		purpleAirService.setCharacteristic(Characteristic.AirQuality, this.transformAQI(aqi));
+
+		this.log.info("PurpleAir %s pm2_5 is %s, AQI is %s, Air Quality is %s.", this.statsKey, pm.toString(), aqi.toString(), this.airQualityString(aqi));
 	},
 
 	adjustPM(pm) {
@@ -167,25 +122,34 @@ PurpleAirAccessory.prototype = {
 
 	calculateAQI: function (pm) {
 		var aqi;
-		var self = this;
 		if (pm > 500) {
 			aqi = 500;
-		} else if (pm > 350.5) {
-			aqi = self.remap(pm, 350.5, 500.5, 400, 500);
-		} else if (pm > 250.5) {
-			aqi = self.remap(pm, 250.5, 350.5, 300, 400);
-		} else if (pm > 150.5) {
-			aqi = self.remap(pm, 150.5, 250.5, 200, 300);
-		} else if (pm > 55.5) {
-			aqi = self.remap(pm, 55.5, 150.5, 150, 200);
-		} else if (pm > 35.5) {
-			aqi = self.remap(pm, 35.5, 55.5, 100, 150);
-		} else if (pm > 12) {
-			aqi = self.remap(pm, 12, 35.5, 50, 100);
-		} else if (pm > 0) {
-			aqi = self.remap(pm, 0, 12, 0, 50);
-		} else { aqi = 0 }
-		return aqi;
+		}
+		else if (pm > 350.5) {
+			aqi = this.remap(pm, 350.5, 500.5, 400, 500);
+		}
+		else if (pm > 250.5) {
+			aqi = this.remap(pm, 250.5, 350.5, 300, 400);
+		}
+		else if (pm > 150.5) {
+			aqi = this.remap(pm, 150.5, 250.5, 200, 300);
+		}
+		else if (pm > 55.5) {
+			aqi = this.remap(pm, 55.5, 150.5, 150, 200);
+		}
+		else if (pm > 35.5) {
+			aqi = this.remap(pm, 35.5, 55.5, 100, 150);
+		}
+		else if (pm > 12) {
+			aqi = this.remap(pm, 12, 35.5, 50, 100);
+		}
+		else if (pm > 0) {
+			aqi = this.remap(pm, 0, 12, 0, 50);
+		}
+		else {
+			aqi = 0
+		}
+		return Math.round(aqi);
 	},
 
 	/**
@@ -197,15 +161,20 @@ PurpleAirAccessory.prototype = {
 		// this.log("Transforming %s.", aqi.toString())
 		if (aqi == undefined) {
 			return (0); // Error or unknown response
-		} else if (aqi <= 50) {
+		}
+		else if (aqi <= 50) {
 			return (1); // Return EXCELLENT
-		} else if (aqi <= 100) {
+		}
+		else if (aqi <= 100) {
 			return (2); // Return GOOD
-		} else if (aqi <= 150) {
+		}
+		else if (aqi <= 150) {
 			return (3); // Return FAIR
-		} else if (aqi <= 200) {
+		}
+		else if (aqi <= 200) {
 			return (4); // Return INFERIOR
-		} else if (aqi > 200) {
+		}
+		else if (aqi > 200) {
 			return (5); // Return POOR (Homekit only goes to cat 5, so combined the last two AQI cats of Very Unhealty and Hazardous.
 		}
 	},
@@ -213,15 +182,20 @@ PurpleAirAccessory.prototype = {
 	airQualityString: function (aqi) {
 		if (aqi == undefined) {
 			return ("Unknown"); // Error or unknown response
-		} else if (aqi <= 50) {
+		}
+		else if (aqi <= 50) {
 			return ("Excellent"); // Return EXCELLENT
-		} else if (aqi <= 100) {
+		}
+		else if (aqi <= 100) {
 			return ("Good"); // Return GOOD
-		} else if (aqi <= 150) {
+		}
+		else if (aqi <= 150) {
 			return ("Fair"); // Return FAIR
-		} else if (aqi <= 200) {
+		}
+		else if (aqi <= 200) {
 			return ("Inferior"); // Return INFERIOR
-		} else if (aqi > 200) {
+		}
+		else if (aqi > 200) {
 			return ("Poor"); // Return POOR (Homekit only goes to cat 5, so combined the last two AQI cats of Very Unhealty and Hazardous.
 		}
 	},
@@ -266,10 +240,8 @@ PurpleAirAccessory.prototype = {
 		purpleAirService.getCharacteristic(Characteristic.AirQuality);
 		purpleAirService.addCharacteristic(Characteristic.StatusFault);
 		purpleAirService.addCharacteristic(Characteristic.PM2_5Density);
-		//      purpleAirService.addCharacteristic(Characteristic.AirQualityIndex);
 		services.push(purpleAirService);
 
 		return services;
 	}
 };
-
