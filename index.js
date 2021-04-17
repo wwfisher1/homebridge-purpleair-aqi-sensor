bcc5c21bb95fa281bb085b0af89e4c56e4ff7327
wwfisher1 <81337802+wwfisher1@users.noreply.github.com>
2021-04-16 17:10:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var Service, Characteristic;
-var request = require('request');
+// var request = require('request');
+var request = require('got');
 
 module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
